9716524f918acc1ce29ea292115c2e9d10136793
Kazim <smkazimn@gmail.com>
2021-03-15 03:22:26 -0400

    fix cookie (#16)
    * fix cookie
    
    * fix typo

--- backend/src/middleware/session.ts
+++ backend/src/middleware/session.ts
@@ -4,6 +4,7 @@ export const sessionParser: RequestHandler = (req, res, next) => {
   req.username = req.session.username ? req.session.username : null;
   res.cookie('username', req.username, {
     maxAge: 60 * 60 * 24 * 7,
+    domain: process.env.ROOT_DOMAIN,
   });
   next();
 };
